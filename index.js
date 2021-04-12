5d2bfeb34e14e15cedad959409b30a6641093467
isabela tassia <isabelatassiasilva@gmail.com>
2021-04-12 18:14:16 -0300

    done

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    this.items.sort((a, b) => a - b);
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.length) {
+      throw new Error("OutOfBounds");
+    }
 
-  max() {}
+    return this.items[pos];
+  }
 
-  min() {}
+  max() {
+    if (this.length == 0) {
+      throw new Error("EmptySortedList");
+    }
+    return Math.max(...this.items);
+  }
 
-  sum() {}
+  min() {
+    if (this.length == 0) {
+      throw new Error("EmptySortedList");
+    }
+    return Math.min(...this.items);
+  }
 
-  avg() {}
+  sum() {
+    let sum = 0;
+    for (let i = 0; i < this.length; i++) {
+      sum += this.items[i];
+    }
+    return sum;
+  }
+
+  avg() {
+    if (this.length == 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.sum() / this.length;
+  }
 }
-
 module.exports = SortedList;
